81c4eb68cdb1738ca5a4e05799598e2c3e1f3caa
Janice Chan <janice.chan1288@gmail.com>
2022-06-19 22:22:01 -0400

    commit tree 424f0b53da35011dcdce807ae51dc9720b8ef2c9

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,46 @@
 class Triangle
-  # write code here
+  attr_reader :a, :b, :c
+
+  def initialize (a, b, c)
+    @a = a
+    @b = b
+    @c = c
+  end
+
+  def kind
+    validate_triangle #run this method first
+    if a == b && b == c
+      :equilateral
+    elsif 
+      a == b || b == c || a == c
+      :isosceles
+    else
+      :scalene   
+    end    
+  end
+
+  def sides_larger_than_0?
+    [a, b, c].all? { |side| side > 0 }
+    # [a, b, c].all?(&:positive?)
+  end
+
+  def sum_of_sides?
+    a + b > c && a + c > b && b + c > a
+  end
+
+  def validate_triangle 
+      if !sides_larger_than_0? && !sum_of_sides?
+      raise TriangleError
+      end    
+    #raise TriangleError unless sides_larger_than_0? && sum_of_sides?      
+  end
+
+    # why doesn't this work?
+    #  if !sides_larger_than_0? && !sum_of_sides?
+    #   raise TriangleError
+    #  end 
+
+    class TriangleError < StandardError
+    end
+
 end
